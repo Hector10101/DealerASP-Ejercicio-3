8cf835a6c3e089fe12a04fe8ecb02bd3259753ba
Hector Sanchez <20186183@itla.edu.do>
2020-02-16 10:28:44 -0400

    Inicio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DealerASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
